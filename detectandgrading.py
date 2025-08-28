53b4d8a8cf1ebc6479cb5c63f07bf36c3c85b8f6
dhdphuong27 <dhdongphuong27@gmail.com>
2025-08-29 06:23:51 +0700

    Update detectandgrading.py

--- detectandgrading.py
+++ detectandgrading.py
@@ -7,16 +7,21 @@ import cv2
 from scan_answer_sheet import AnswerSheetScanner
 from calculate_score import grade_exam
 
-image_paths = glob.glob(os.path.join("input", "*.*"))  # Matches any file type
+
+image_paths = glob.glob(os.path.join("input", "*.*")) 
 images = []
 for path in image_paths:
     img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    if img is not None:  # Skip if file isn't an image
+    if img is not None:  
       images.append(img)
   
 scanner = AnswerSheetScanner(SAVE_SCAN_RESULT=True)
 for image in images:
   result = scanner.scan_answer(image)
 
-grade_exam("output/dap_an.csv", "output/*_result.csv")
+if len(sys.argv) > 1:
+    input_text = sys.argv[1]
+    grade_exam("output/"+input_text+".csv", "output/*_result.csv")
+
+
 
